d99f9d11de1ea148316e5603d354791e0cec29fd
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-04-29 12:50:06 -0400

    fix: Use the correct equation for Binet formula in calculate-fibonacci-lru-cache.

--- source/calculate-fibonacci-lru-cache.py
+++ source/calculate-fibonacci-lru-cache.py
@@ -17,22 +17,22 @@ def fibonacci_binet(n: int) -> int:
     # https://zeptomath.com/tools/fibonaccinumbers.php?number=150&hl=en
     # https://artofproblemsolving.com/wiki/index.php/Binet%27s_Formula
     # https://medium.com/explorations-in-python/calculating-any-term-of-the-fibonacci-sequence-using-binets-formula-in-python-36e7e261d1d8
-    square_root_n = math.sqrt(n)
-    return int((((1 + square_root_n) ** n - (1 - square_root_n) ** n) / (2 ** n * square_root_n)))
+    square_root_five = math.sqrt(5)
+    return int((((1 + square_root_five) ** n - (1 - square_root_five) ** n) / (2 ** n * square_root_five)))
 
 
 if __name__ == "__main__":
     # request the 100th fibonacci number while using the LRU cache
     fibonacci_value_100 = fibonacci(100)
-    print(f"Recursive fibonacci(100) = {fibonacci_value_100}")
-    print(f"LRU cache information: {fibonacci.cache_info()}")
+    print(f"Recursive Fibonacci(100) = {fibonacci_value_100}")
     # confirm the calculation of Fibonacci number with Binet's equation
     fibonacci_value_binet_100 = fibonacci_binet(100)
-    print(f"Binet fibonacci(100) = {fibonacci_value_100}")
+    print(f"Binet Fibonacci(100)     = {fibonacci_value_binet_100}")
+    print(f"LRU cache information: {fibonacci.cache_info()}")
     # request the 150th fibonacci number while using the LRU cache
     fibonacci_value_150 = fibonacci(150)
-    print(f"fibonacci(150) = {fibonacci_value_150}")
-    print(f"LRU cache information: {fibonacci.cache_info()}")
     # confirm the calculation of Fibonacci number with Binet's equation
     fibonacci_value_binet_150 = fibonacci_binet(150)
-    print(f"Binet fibonacci(150) = {fibonacci_value_150}")
+    print(f"Recursive Fibonacci(150) = {fibonacci_value_150}")
+    print(f"Binet Fibonacci(150) = {fibonacci_value_binet_150}")
+    print(f"LRU cache information: {fibonacci.cache_info()}")
